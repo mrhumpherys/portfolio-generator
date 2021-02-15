0877a1a8ff93b613cd042bb1a6698d0b72812b4b
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-02-15 17:11:14 -0500

    generate webpage functional

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs
 //console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log("========");
+//const printProfileData = (profileDataArr) => {
+//    for (let i = 0; i < profileDataArr.length; i += 1) {
+ //       console.log(profileDataArr[i]);
+ //   }
+//    console.log("========");
     
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+ //   profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if(err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
