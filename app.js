e7bfe4227cca3ff92583f10bb2bbaa12199bfbf6
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-02-17 12:19:16 -0500

    inquirer added

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+//const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs
+//fs.writeFile("./index.html", pageHTML, err => {
+    //if(err) throw err;
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
 //console.log(profileDataArgs);
 
 //const printProfileData = (profileDataArr) => {
@@ -16,8 +30,3 @@ const [name, github] = profileDataArgs
 //};
 
 //printProfileData(profileDataArgs);
-
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if(err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
