6c6aeca06efde9a4a3f47c11107879f23b075c1c
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-02-18 15:35:06 -0500

    updated template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,14 +134,14 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-    });
-//const pageHTML = generatePage(name, github);
+        const pageHTML = generatePage(portfolioData);
 
-//fs.writeFile("./index.html", pageHTML, err => {
-    //if(err) throw err;
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
+        fs.writeFile("./index.html", pageHTML, err => {
+            if(err) throw err;
+                console.log('Portfolio complete! Check out index.html to see the output!');
+        });
+    });
+
 
 //console.log(profileDataArgs);
 
